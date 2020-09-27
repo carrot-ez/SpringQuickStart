127741b1cc7ce88a0d011f3a9d92c8d6f6f97dfc
CarrotNuts <kain6245@gmail.com>
2020-09-28 00:57:52 +0900

    integrate board controller

--- src/main/java/com/springbook/view/board/BoardController.java
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -1,5 +1,49 @@
 package com.springbook.view.board;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.board.impl.BoardDAO;
+
+@Controller
 public class BoardController {
 
+	// get board list
+	@RequestMapping("/getBoardList.do")
+	public ModelAndView getBoardList(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
+		mav.addObject("boardList", boardDAO.getBoardList(vo)); // model
+		mav.setViewName("getBoardList.jsp"); // view
+		return mav;
+	}
+	
+	// get board
+	@RequestMapping("/getBoard.do")
+	public ModelAndView getBoard(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
+		mav.addObject("board", boardDAO.getBoard(vo));// model
+		mav.setViewName("getBoard.jsp");
+		return mav;
+	}
+	
+	// insert board
+	@RequestMapping("/insertBoard.do")
+	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
+		boardDAO.insertBoard(vo);
+		return "getBoardList.do";
+	}
+	
+	// delete board
+	@RequestMapping("/deleteBoard.do")
+	public String deleteBoard(BoardVO vo, BoardDAO boardDAO) {
+		boardDAO.deleteBoard(vo);
+		return "getBoardList.do";
+	}
+	
+	// update board
+	@RequestMapping("/updateBoard.do")
+	public String updateBoard(BoardVO vo, BoardDAO boardDAO) {
+		boardDAO.updateBoard(vo);
+		return "getBoardList.do";
+	}
 }
