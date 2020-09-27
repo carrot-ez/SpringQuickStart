eebb97392b59c4e02eed0d0cc33c2b45a99a994c
CarrotNuts <kain6245@gmail.com>
2020-09-28 00:24:04 +0900

    Update Controller using Annotation

--- src/main/java/com/springbook/view/board/GetBoardController.java
+++ src/main/java/com/springbook/view/board/GetBoardController.java
@@ -1,32 +1,19 @@
 package com.springbook.view.board;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
-public class GetBoardController implements Controller {
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
-		// 1. 검색할 게시판 번호 추출
-		String seq = request.getParameter("seq");
-
-		// 2. DB 연동 처리
-		BoardVO vo = new BoardVO();
-		vo.setSeq(Integer.parseInt(seq));
-		
-		BoardDAO boardDAO = new BoardDAO();
-		BoardVO board = boardDAO.getBoard(vo);
-		
-		// 3. 검색 결과를 세션에 저장하고 상세 화면으로 이동한다.
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("board", board); // model
-		mav.setViewName("getBoard"); // view
+@Controller
+public class GetBoardController {
+	
+	@RequestMapping("/getBoard.do")
+	public ModelAndView getBoard(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
+		mav.addObject("board", boardDAO.getBoard(vo));// model
+		mav.setViewName("getBoard.jsp");
 		return mav;
 	}
-
 }
--- src/main/java/com/springbook/view/board/GetBoardListController.java
+++ src/main/java/com/springbook/view/board/GetBoardListController.java
@@ -1,31 +1,19 @@
 package com.springbook.view.board;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
-public class GetBoardListController implements Controller{
-	
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
-		// 1. 사용자 입력 정보 추출
-		// 2. DB 연동 처리
-		BoardVO vo = new BoardVO();
-		BoardDAO boardDAO = new BoardDAO();
-		List<BoardVO> boardList = boardDAO.getBoardList(vo);
-		
-		// 3. 검색 결과를 세션에 저장하고 목록 화면으로 이동한다.
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("boardList", boardList);
-		mav.setViewName("getBoardList");
+@Controller
+public class GetBoardListController {
+
+	@RequestMapping("/getBoardList.do")
+	public ModelAndView getBoardList(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
+		mav.addObject("boardList", boardDAO.getBoardList(vo)); // model
+		mav.setViewName("getBoardList.jsp"); // view
 		return mav;
 	}
-
 }
